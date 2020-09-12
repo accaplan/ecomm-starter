9240bbf7e216e8959cc1db94eb2770c79ef460fe
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-09-12 10:15:29 -0700

    add styled components

---
+++ src/components/ProductCard/ProductCard.Styled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const ProductCard = styled.div`
+  border: 1px solid blue;
+  width: 100%;
+  max-width: 30em;
+`;
+
+export default {
+  ProductCard,
+};
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,15 +1,17 @@
 import { AddToCartButton, useProduct } from "@tylermcrobert/shopify-react";
 import React from "react";
+import S from "./ProductCard.Styled";
 
 const ProductCard = () => {
   const { product } = useProduct();
   console.log(product.vendor);
 
   return (
-    <div>
+    <S.ProductCard>
       {product.title}
+      <img src={product.images[0].src} />
       <AddToCartButton />
-    </div>
+    </S.ProductCard>
   );
 };
 
