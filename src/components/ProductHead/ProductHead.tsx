5bf379f86d3b0835c637fec9d997223a1f7e822f
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-09-15 15:09:39 -0700

    add variant select

--- src/components/ProductHead/ProductHead.tsx
+++ src/components/ProductHead/ProductHead.tsx
@@ -1,4 +1,3 @@
-import { ProductOptions } from "@tylermcrobert/shopify-react";
 import { useProduct } from "@tylermcrobert/shopify-react";
 import { QtySelect } from "components";
 import React from "react";
@@ -14,6 +13,7 @@ const ProductHead = () => {
       </div>
       <div>
         <h1>{product.title}</h1>
+        <div>${productState.currentVariant.price}</div>
         <ProductOptions />
         <QtySelect
           onUpdate={(num) => setQuantity(num)}
@@ -28,4 +28,67 @@ const ProductHead = () => {
   );
 };
 
+const ProductOptions = () => {
+  const { product, productState, setOptions } = useProduct();
+
+  return (
+    <div>
+      {product.options.map(({ name, values }) => {
+        const isSelected = (optionName: string, optionValue: string) =>
+          productState.currentVariant.selectedOptions.filter(
+            (option) => option.name === optionName
+          )[0].value === optionValue;
+
+        return (
+          <div key={name}>
+            {name !== "Title" && <div>{name}</div>}
+            {values.map(({ value }) => {
+              const selected = isSelected(name, value);
+              return (
+                value !== "Default Title" && (
+                  <VariantSelect
+                    key={value}
+                    onChange={() => setOptions({ [name]: value })}
+                    value={value}
+                    selected={selected}
+                  />
+                )
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+/**
+ * VariantSelect item
+ * @param Props
+ */
+
+const VariantSelect: React.FC<
+  React.HTMLAttributes<HTMLDivElement> & {
+    /** Text value for label */
+    value: string;
+    /** Function that is called on happens on select */
+    onChange: () => any;
+    /** Is option active */
+    selected: boolean;
+  }
+> = ({ onChange, value, selected, ...props }) => {
+  return (
+    <div {...props}>
+      <input
+        type="radio"
+        name={name}
+        value={value}
+        id={value}
+        checked={selected}
+        onChange={onChange}
+      />
+      <label htmlFor={value}>{value}</label>
+    </div>
+  );
+};
 export default ProductHead;
