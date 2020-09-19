e1e253427586cc136f8f9c4259b8aacbfc5d40a2
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-09-18 18:43:12 -0700

    update sync

--- src/pages/api/shopify-sync.ts
+++ src/pages/api/shopify-sync.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "POST" || !req.body) {
-    res.json({ error: true, message: "Must be post method with body" });
-  }
+  // if (req.method !== "POST" || !req.body) {
+  //   res.json({ error: true, message: "Must be post method with body" });
+  // }
 
   console.log(req.body);
   res.json({ success: true });
