32534170a8caddb37fade64b7bf2e23514e2bec4
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-09-25 09:34:57 -0700

    add pdp

--- src/pages/products/[handle].tsx
+++ src/pages/products/[handle].tsx
@@ -1,27 +1,41 @@
 import { Product, ProductProvider } from "@tylermcrobert/shopify-react";
 import { Layout } from "components";
 import ProductHead from "components/ProductHead/ProductHead";
+import { fetchProduct } from "lib/sanity";
 import { NextPage } from "next";
 import { client } from "pages/_app";
 import React from "react";
+import { ProductSchema } from "types";
+import Error from "next/error";
 
-const PDP: NextPage<{ product: Product }> = ({ product }) => {
+const PDP: NextPage<{ product: Product; cmsProduct: ProductSchema }> = ({
+  product,
+  cmsProduct,
+}) => {
+  if (!product || !cmsProduct) {
+    if (!product) console.error("Cannot find Product from Shopify Buy SDK.");
+    if (!cmsProduct) console.error("Cannot find Product from Sanity.");
+    return <Error statusCode={404} />;
+  }
   return (
     <Layout>
-      <ProductProvider product={product}>
-        <ProductHead />
-      </ProductProvider>
+      {/* <ProductProvider product={product}> */}
+      {/* <ProductHead /> */}
+      {/* </ProductProvider> */}
     </Layout>
   );
 };
 
 PDP.getInitialProps = async (ctx) => {
-  const product: Product = JSON.parse(
-    JSON.stringify(
-      await client.product.fetchByHandle(ctx.query.handle?.toString())
-    )
+  const product = await client.product.fetchByHandle(
+    ctx.query.handle?.toString()
   );
-  return { product };
+
+  const cmsProduct = await fetchProduct(
+    ctx.query.handle?.toString() || "",
+    false
+  );
+  return { product, cmsProduct };
 };
 
 export default PDP;
