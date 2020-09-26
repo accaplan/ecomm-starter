3d6d0c6020f94b67cac7b43902b438ab8f6d6173
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-09-25 17:59:43 -0700

    catch sync errors

--- src/pages/api/shopify-sync.ts
+++ src/pages/api/shopify-sync.ts
@@ -50,120 +50,129 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return null;
   };
 
-  /**
-   * Validate
-   */
+  try {
+    /**
+     * Validate
+     */
 
-  if (req.method !== "POST") setError(400, "Must be POST method");
-  if (!req.body) setError(400, "Must include body");
+    if (req.method !== "POST") setError(400, "Must be POST method");
+    if (!req.body) setError(400, "Must include body");
 
-  /**
-   * Extract data from body payload
-   */
+    /**
+     * Extract data from body payload
+     */
 
-  const data = req.body as ShopifyWebhookRes;
+    const data = req.body as ShopifyWebhookRes;
 
-  const incomingOptions: SanityProductOption[] = data.options.map((item) => ({
-    _key: item.id.toString(),
-    _type: "option",
-    name: item.name,
-    values: item.values.map((value) => ({
-      _key: value,
-      _type: "value",
-      title: value,
-    })),
-  }));
-
-  const incomingVariants = data.variants.map((variant) => ({
-    id: variant.id.toString(),
-    title: variant.title,
-    _key: variant.id.toString(),
-  }));
-
-  /**
-   * Fetch existing product & make sure it isn't a draft
-   */
-
-  const existingSanityProducts: ProductSchema[] | null =
-    (await client.fetch(`*[_type == "product" && slug.current == $slug]`, {
-      slug: data.handle.toString(),
-    })) || null;
-
-  if ((existingSanityProducts && existingSanityProducts.length > 1) || false) {
-    setError(
-      503,
-      `Sanity document ${data.title} is a draft. Publish the document to update with sync.`
-    );
-    return null;
-  }
-
-  const existingSanityProduct =
-    (existingSanityProducts && existingSanityProducts[0].shopify) || null;
-
-  /**
-   * Merge variants and Options
-   */
-
-  const mergedVariants = incomingVariants.map((newVariant) => ({
-    ...newVariant,
-    ...existingSanityProduct?.variants?.find(
-      (oldVariant) => oldVariant.id === newVariant.id
-    ),
-  }));
-
-  const mergedOptions = incomingOptions.map((incomingOption) => {
-    /** Get data from existing option */
-    const existingOption = existingSanityProduct?.options?.find(
-      (existingOption) => existingOption.name === incomingOption.name
-    );
-
-    return {
-      /** add option from webhook*/
-      ...incomingOption,
-      /** overwrite webhook option with cms option if it exists */
-      ...existingOption,
-      /** Update values by deep-merging */
-      values: incomingOption.values?.map((incomingVal) => ({
-        /** Add value from webhook */
-        ...incomingVal,
-        /** Overwrite if already exists in CMS */
-        ...existingOption?.values?.find((val) => val._key === incomingVal._key),
-        _key: incomingVal.title,
+    const incomingOptions: SanityProductOption[] = data.options.map((item) => ({
+      _key: item.id.toString(),
+      _type: "option",
+      name: item.name,
+      values: item.values.map((value) => ({
+        _key: value,
+        _type: "value",
+        title: value,
       })),
-    };
-  });
+    }));
 
-  /**
-   * New Product
-   */
+    const incomingVariants = data.variants.map((variant) => ({
+      id: variant.id.toString(),
+      title: variant.title,
+      _key: variant.id.toString(),
+    }));
 
-  const product = {
-    _type: "product",
-    _id: data.id.toString(),
-    title: data.title,
-    slug: {
-      _type: "slug",
-      current: data.handle,
-    },
-    shopify: {
-      variants: mergedVariants,
-      options: mergedOptions,
-    },
-    data: {
-      price: data.variants[0].price,
-    },
-  };
+    /**
+     * Fetch existing product & make sure it isn't a draft
+     */
 
-  return client
-    .transaction()
-    .createIfNotExists(product)
-    .patch(data.id.toString(), (patch) => patch.set(product))
-    .commit()
-    .then((resp) => {
-      sendToSlack(`Successfully pushed ${data.title} to Sanity`);
-      res.status(200).json(resp);
-    })
-    .catch((err) => {
-      setError(400, "Error pushing to Sanity", err);
+    const existingSanityProducts: ProductSchema[] | null =
+      (await client.fetch(`*[_type == "product" && slug.current == $slug]`, {
+        slug: data.handle.toString(),
+      })) || null;
+
+    if (
+      (existingSanityProducts && existingSanityProducts.length > 1) ||
+      false
+    ) {
+      setError(
+        503,
+        `Sanity document ${data.title} is a draft. Publish the document to update with sync.`
+      );
+      return null;
+    }
+
+    const existingSanityProduct =
+      (existingSanityProducts && existingSanityProducts[0]?.shopify) || null;
+
+    /**
+     * Merge variants and Options
+     */
+
+    const mergedVariants = incomingVariants.map((newVariant) => ({
+      ...newVariant,
+      ...existingSanityProduct?.variants?.find(
+        (oldVariant) => oldVariant.id === newVariant.id
+      ),
+    }));
+
+    const mergedOptions = incomingOptions.map((incomingOption) => {
+      /** Get data from existing option */
+      const existingOption = existingSanityProduct?.options?.find(
+        (existingOption) => existingOption.name === incomingOption.name
+      );
+
+      return {
+        /** add option from webhook*/
+        ...incomingOption,
+        /** overwrite webhook option with cms option if it exists */
+        ...existingOption,
+        /** Update values by deep-merging */
+        values: incomingOption.values?.map((incomingVal) => ({
+          /** Add value from webhook */
+          ...incomingVal,
+          /** Overwrite if already exists in CMS */
+          ...existingOption?.values?.find(
+            (val) => val._key === incomingVal._key
+          ),
+          _key: incomingVal.title,
+        })),
+      };
     });
+
+    /**
+     * New Product
+     */
+
+    const product = {
+      _type: "product",
+      _id: data.id.toString(),
+      title: data.title,
+      slug: {
+        _type: "slug",
+        current: data.handle,
+      },
+      shopify: {
+        variants: mergedVariants,
+        options: mergedOptions,
+      },
+      data: {
+        price: data.variants[0].price,
+      },
+    };
+
+    return client
+      .transaction()
+      .createIfNotExists(product)
+      .patch(data.id.toString(), (patch) => patch.set(product))
+      .commit()
+      .then((resp) => {
+        sendToSlack(`Successfully pushed ${data.title} to Sanity`);
+        res.status(200).json(resp);
+      })
+      .catch((err) => {
+        setError(400, "Error pushing to Sanity", err);
+      });
+  } catch (err) {
+    setError(500, "Unknown error posting to Sanity", err);
+  }
 };
